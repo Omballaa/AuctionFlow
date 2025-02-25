cd320ecf5acbb1b0f0c85fa1386ab8d5ff86a684
Wh1tefoxu <152866883+renardtheo@users.noreply.github.com>
2025-02-25 09:54:54 +0100

    Correction Implémentation Java

---
+++ AuctionFlow/src/main/java/fr/eni/auctionflow/AuctionFlowApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.auctionflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionFlowApplication.class, args);
+	}
+
+}
---
+++ AuctionFlow/src/test/java/fr/eni/auctionflow/AuctionFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.auctionflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

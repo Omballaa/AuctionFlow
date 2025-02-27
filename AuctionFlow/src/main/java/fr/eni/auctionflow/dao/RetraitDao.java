4c248fec3148aef2d4fe3551fa49ba37036d6b7c
Kieran <kieran.godefroid@gmail.com>
2025-02-27 15:06:52 +0100

    spécification @Repository dans les dao

--- AuctionFlow/src/main/java/fr/eni/auctionflow/dao/RetraitDao.java
+++ AuctionFlow/src/main/java/fr/eni/auctionflow/dao/RetraitDao.java
@@ -1,10 +1,9 @@
 package fr.eni.auctionflow.dao;
 
-import jdk.jfr.Enabled;
 import org.springframework.data.jpa.repository.JpaRepository;
 import fr.eni.auctionflow.model.Retrait;
 import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface RetraitDao extends JpaRepository<Retrait, Long> {
 }
--- AuctionFlow/src/main/java/fr/eni/auctionflow/dao/UtilisateurDao.java
+++ AuctionFlow/src/main/java/fr/eni/auctionflow/dao/UtilisateurDao.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import fr.eni.auctionflow.model.Utilisateur;
 import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface UtilisateurDao extends JpaRepository<Utilisateur, Long> {
     Utilisateur findByPseudo(String pseudo);
     Utilisateur findByEmail(String email);

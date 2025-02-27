9cedb7981305fb4e6e6b6453dd76ea32929e6c61
Kieran <kieran.godefroid@gmail.com>
2025-02-27 11:11:14 +0100

    Ajout des models et dao dans le main app + correction de hibernate (creation des tables) tests OK

--- AuctionFlow/src/main/java/fr/eni/auctionflow/AuctionFlowApplication.java
+++ AuctionFlow/src/main/java/fr/eni/auctionflow/AuctionFlowApplication.java
@@ -2,8 +2,13 @@ package fr.eni.auctionflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 
 @SpringBootApplication
+@EntityScan("com.eni.encheres.model") 
+@EnableJpaRepositories("com.eni.encheres.dao")
 public class AuctionFlowApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +16,6 @@ public class AuctionFlowApplication {
 	}
 
 }
+
+
+
